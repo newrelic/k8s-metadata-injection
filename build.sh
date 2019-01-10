a8e4dcf94f457545c84ca50fdd6bde96db557e6d
Magdalena Pajda <mpajda@newrelic.com>
2019-01-10 15:25:48 +0100

    Change name of binary file

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ REGISTRY=docker.coscale.com:5000
 IMAGE=fryckbosch/newrelic-k8s-metadata-injector:v1
 
 dep ensure
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o k8s-env-inject .
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o k8s-metadata-injection .
 docker build --no-cache -t ${REGISTRY}/${IMAGE} .
-rm -rf k8s-env-inject
+rm -rf k8s-metadata-injection
 
 docker push ${REGISTRY}/${IMAGE}
