cc8da51ff15a468c754e0b3e60ccc11ead226db8
Douglas Camata <dcamata@newrelic.com>
2019-01-16 19:22:02 +0100

    Update the build script for go modules

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
-REGISTRY=docker.coscale.com:5000
 IMAGE=quay.io/newrelic/k8s-metadata-injector-dev:latest
 
-dep ensure
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o k8s-metadata-injection .
-docker build --no-cache -t ${REGISTRY}/${IMAGE} .
+docker build --no-cache -t ${IMAGE} .
 rm -rf k8s-metadata-injection
