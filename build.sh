ca2f8c86a4004e252c7eb7dcf20271a7c8b0b485
Douglas Camata <dcamata@newrelic.com>
2019-01-21 12:17:36 +0100

    Remove unused build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-IMAGE=quay.io/newrelic/k8s-metadata-injector-dev:latest
-
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o k8s-metadata-injection .
-docker build --no-cache -t ${IMAGE} .
-rm -rf k8s-metadata-injection
