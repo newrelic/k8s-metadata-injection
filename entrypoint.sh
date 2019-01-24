b1b75b2c16ee0d33bd6dc3fd5000518d1321a787
Sergio Moya <smoya@newrelic.com>
2019-01-24 16:49:47 +0100

    Improving readability in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -e
 
-exec NEW_RELIC_K8S_METADATA_INJECTION_CLUSTER_NAME=${clusterName} /app/k8s-metadata-injection 2>&1
+export NEW_RELIC_K8S_METADATA_INJECTION_CLUSTER_NAME=${clusterName}
+exec /app/k8s-metadata-injection 2>&1
