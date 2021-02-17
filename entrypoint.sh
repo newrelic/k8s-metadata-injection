3b50c1b738954fee135c81f4d5ae1ee3897812c4
Roberto Santalla <roobre@roobre.es>
2021-02-17 17:47:17 +0100

    remove test scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
 export NEW_RELIC_K8S_METADATA_INJECTION_CLUSTER_NAME=${clusterName}
-# TODO remove
-stat /app/k8s-metadata-injection
-ldd /app/k8s-metadata-injection || true
+
 exec /app/k8s-metadata-injection 2>&1
