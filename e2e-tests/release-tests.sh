281b8cff4560ebcdd79e912d047a61b9ef9f089c
chris <csong@newrelic.com>
2023-11-17 12:08:33 -0800

    optionalargs

--- e2e-tests/release-tests.sh
+++ e2e-tests/release-tests.sh
@@ -12,9 +12,17 @@ DUMMY_DEPLOYMENT_NAME="dummy-deployment"
 DUMMY_POD_LABEL="app=${DUMMY_DEPLOYMENT_NAME}"
 ENV_VARS_PREFIX="NEW_RELIC_METADATA_KUBERNETES"
 NAMESPACE_NAME="$(kubectl config view --minify --output 'jsonpath={..namespace}')"
+IMAGE_TAG="e2e-test"
 
-IMAGE_TAG="$(curl --silent "https://api.github.com/repos/newrelic/k8s-metadata-injection/releases" | jq -r 'map(select(.prerelease)) | first | .tag_name')"
-IMAGE_TAG="${IMAGE_TAG:1}-pre"
+while getopts i: flag 
+do 
+    case "${flag}" in 
+        i) IMAGE_TAG=${OPTARG};; 
+    esac
+done 
+
+# IMAGE_TAG="$(curl --silent "https://api.github.com/repos/newrelic/k8s-metadata-injection/releases" | jq -r 'map(select(.prerelease)) | first | .tag_name')"
+# IMAGE_TAG="${IMAGE_TAG:1}-pre"
 
 finish() {
     printf "webhook logs:\n"
